b17fee2ff89f3e60cb8b9899dbd0be48ea8122d6
zhuxuanhe <50013066+zhuxuanhe@users.noreply.github.com>
2021-06-01 22:26:57 +0800

    Create willdelete.py

---
+++ willdelete.py
@@ -0,0 +1,30 @@
+
+class tea:
+    
+    charsh=0
+    priere=[]
+    def __init__(self,haweaw):
+        #print('yes')
+        self.charsh=0
+        self.charsh=haweaw
+    
+    def hewawe(self):
+        self.charsh+=1
+    
+    def pritn(self):
+        self.charsh-=1
+
+    def pritnt(self):
+        print(self.charsh)
+
+p = tea(0)
+
+p.hewawe()
+
+p.hewawe()
+
+p.pritnt()
+
+#p.
+
+
